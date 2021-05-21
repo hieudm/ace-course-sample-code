0511d95a15953574adacd46aedbd23f855117870
Dao Manh Hieu <kienvang@gmail.com>
2021-05-21 17:54:16 +0700

    demo source code for ACE Class

---
+++ demo3Java/utils/GenerateName.java
@@ -0,0 +1,13 @@
+package utils;
+
+public class GenerateName {
+
+	/**
+	 * Sample method that can be called from a Mapping Custom Java transform.
+	 * The content of this method provides the implementation for the Custom Java transform.
+	 */
+	public static String generateName(String inputName) {
+		return "Name from java id = " + inputName;
+	}
+
+}
